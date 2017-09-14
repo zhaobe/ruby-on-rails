8aba1789ecb4bcae85000f88d890daf155a4abcf
zhaobe <zhaoben777@gmail.com>
2017-09-14 00:12:19 -0700

    Added methods to controller, added call to uploaded in resume.rb and modified routes.rb.

--- testfile/app/models/resume.rb
+++ testfile/app/models/resume.rb
@@ -1,2 +1,5 @@
-class Resume < ApplicationRecord
+#class Resume < ApplicationRecord
+class Resume < ActiveRecord::Base
+  mount_uploader :attachment, AttachmentUploader # For rails to use this uploader for this model.
+  validates :name, presence: true # Verify owner's name is present.
 end
--- testfile/config/routes.rb
+++ testfile/config/routes.rb
@@ -1,3 +1,8 @@
-Rails.application.routes.draw do
+#Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#end
+
+CarrierWaveExample::Application.routes.draw do
+   resources :resumes, only: [:index, :new, :create, :destroy]
+   root "resumes#index"
 end
