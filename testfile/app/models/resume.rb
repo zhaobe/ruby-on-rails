9093ea46a1e976eded65effdb2caa64be967b2c5
zhaobe <zhaoben777@gmail.com>
2017-09-15 23:56:40 -0700

    Nuking testfile project and redo.

--- testfile/app/models/resume.rb
+++
@@ -1,5 +0,0 @@
-#class Resume < ApplicationRecord
-class Resume < ActiveRecord::Base
-  mount_uploader :attachment, AttachmentUploader # For rails to use this uploader for this model.
-  validates :name, presence: true # Verify owner's name is present.
-end
